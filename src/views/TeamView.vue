daf905548837843939f9f373cf8a66ab92704d69
devHenryKimani <kimhenry756@gmail.com>
2025-06-27 12:34:15 +0300

    v-show

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -7,13 +7,13 @@ import team from "@/team.json";
 <template>
   <header class="flex justify-between">
     <div>
-      <button class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400" :disabled="true">Add Member (0 Spots Left)</button>
+      <button class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400" :disabled="team.members.length== team.spots">Add Member ({{team.spots-team.members.length  }} Spots Left)</button>
     </div>
     <div>
       <div class="inline-flex items-center text-3xl relative">
         <img src="/smiley.png" alt="" class="mr-2">
         <h3>{{ team.name }} Team</h3>
-        <div class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2">5</div>
+        <div class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2">{{ team.spots }}</div>
       </div>
     </div>
   </header>
@@ -34,7 +34,7 @@ import team from "@/team.json";
       
       </tbody>
     </table>
-    <p class="text-right text-gray-600 italic">There are no remaining team spots. Upgrade to add more.</p>
+    <p class="text-right text-gray-600 italic " v-show="team.members.length==team.spots">There are no remaining team spots. Upgrade to add more.</p>
   </div>
   <footer class="mt-12 bg-gray-100 py-4 text-center">
     <h5 class="font-semibold text-lg">{{ team.name }} - {{ team.spot }}Member Team</h5>
